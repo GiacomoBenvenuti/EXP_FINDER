49fcf356880e38796eecea449935d645a1800bb0
Giac <giacomox@gmail.com>
2017-04-20 10:04:19 -0500

    does the job

---
+++ initGUI.m
@@ -0,0 +1,25 @@
+function [MonkName, ProtNames,MN] = initGUI
+% Generate the total Monkeys and Protocols list (names/cell)
+% by GB
+s=what('EXP_FINDER');
+cd (s.path);
+cd DATABASE
+
+MN= dir('*PList.mat');
+MN2 = {MN.name};
+for i = 1:size(MN2,2)
+    tmp =MN2{i};
+    tmp2=tmp(1:size(tmp,2)-9);
+    MonkName{i} = tmp2;
+end
+ 
+PRT= dir('PRT*');
+tm = {PRT.name};
+for i =1:size(tm,2)
+    PR{i} = tm{i}(5:end)
+end
+
+ProtNames = PR;
+
+end
+

dd0d48ba99007d14f43da5d40ccc4dec25c6f4eb
Giac <giacomox@gmail.com>
2017-06-01 12:34:11 -0500

    solved a bug in the generation of the Plist. Using /try/ together with a counter, I was misalign the index of the experiments adresses correspondig to a specific Protocol

--- TempoDataSummary.m
+++ TempoDataSummary.m
@@ -10,30 +10,43 @@ function TempoDataSummary(DataFolder)
 % Find Mokeyname from folders names
 myfold = dir([DataFolder filesep '*LogAddress.mat']);
 a={myfold.name};
-           
-            
+
+
 for i =1:size(a,2)
     
-load([DataFolder filesep a{i}])
-fn = LogAddress;
+    load([DataFolder filesep a{i}])
+    fn = LogAddress;
     
     % MONKEY PROTOCOLS SUMMARY
     % go through all experiment and generate a structure var
     % with a field for each protocol and the list of blocks id
     clear AllProt
+    AllProt= repmat({'None'}, 1,size(fn,2));
+  
     ctt=0;
     for y =1:size(fn,2) % go through all experiments
         clear A tm
-        if isempty(strfind(fn{y}(1),'.'))
-         ctt=ctt+1;
-        A = fileread( fn{y});
-        tstart = strfind(A,'ProtocolName=') + numel('ProtocolName=')+1;
-        tend = strfind(A(tstart:tstart+50),'''') + tstart-2;
-        
-        AllProt{ctt} = A(tstart:tend);
+        if  ~strcmp(fn{y},'')
+            if isempty(strfind(fn{y}(1),'.'))
+                try
+                    
+                    A = fileread( fn{y});
+                    tstart = strfind(A,'ProtocolName=') + numel('ProtocolName=')+1;
+                   if  ~isempty(tstart)
+                    tend = strfind(A(tstart:tstart+50),'''') + tstart-2;
+                   % ctt=ctt+1;
+                   % AllProt{ctt} = A(tstart:tend);
+              
+                 
+                     AllProt{y} = A(tstart:tend);
+                   end
+                end
+                
+            end
         end
     end
     
+    if  ~isempty(AllProt)
     clear AP
     [AP x APN]= unique(AllProt);
     clear Plist
@@ -53,13 +66,16 @@ fn = LogAddress;
         end
     end
     
+    Plist = rmfield(Plist,'None');
+    
     tm = size(a{i},2);
-    MonkName = a{i}(1:tm-14);  
+    MonkName = a{i}(1:tm-14);
     eval([MonkName 'PList= Plist']);
     
     save([DataFolder filesep...
         MonkName 'PList'], [MonkName 'PList'] )
-end
+    end
+ end
 
 end
 
